19f405328a972bdff6bd1abbd6052b66e9ef2012
FabricioF97 <faa.ferreira97@gmail.com>
2025-11-13 21:11:02 -0300

    Colocando animação

--- script.js
+++ script.js
@@ -30,3 +30,19 @@ habilidades.addEventListener("click", function() {
         estudando.style.color = "rgb(43, 42, 42)";
     }
 })
+const img = document.getElementById('foto');
+
+img.tabIndex= 0 ;
+
+
+img.addEventListener('mouseenter', () =>img.classList.add('is-expanded'))
+img.addEventListener('mouseleave', () =>img.classList.remove('is-expanded'))
+img.addEventListener('click', () =>{img.classList.toggle('is-expanded')}) 
+img.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+        
+        img.classList.toggle('is-expanded');
+    }
+   
+    e.preventDefault();
+});
